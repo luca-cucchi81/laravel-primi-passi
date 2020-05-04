81a2c4667910727ebb8209e61edf6da7067a61bd
Luca <lu.cucchi@gmail.com>
2020-05-04 16:45:15 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -153,7 +153,7 @@
             <img src="{{asset('img/logo.png')}}" alt="logo">
             <nav class="navbar">
                 <ul class="main-menu">
-                    <li><a href="#">Home</a></li>
+                    <li><a href="{{route('home')}}">Home</a></li>
                     <li><a href="#">Prodotti</a></li>
                     <li><a href="#">News</a></li>
                 </ul>
