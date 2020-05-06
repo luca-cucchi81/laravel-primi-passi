667a5f2e28a4ee6149d6a4d3a857fc9fd48f16c6
Luca <lu.cucchi@gmail.com>
2020-05-06 14:34:35 +0200

    added and setup layouts / config

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-        @include('partials.header')
-        @include('partials.footer')
-    </body>
-</html>
+@extends('layouts.index_layout')
+@section('titolo')
+    Home
+@endsection
