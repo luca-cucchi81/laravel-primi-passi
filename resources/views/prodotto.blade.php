f13c3d79f3f44b7c4870b38b314b412dad11c51d
Luca <lu.cucchi@gmail.com>
2020-05-07 16:30:33 +0200

    added arrows

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -12,8 +12,10 @@
 @section('main')
     <div class="container2">
         <h2>{{$articolo['titolo']}}</h2>
-        <img src="{{$articolo['src-h']}}" alt="">
-        <img src="{{$articolo['src-p']}}" alt="">
+        <img src="{{$articolo['src-h']}}" alt="{{$articolo['titolo']}}">
+        <img src="{{$articolo['src-p']}}" alt="{{$articolo['titolo']}}">
+        <a href=""><img id="arrow-left" src="{{asset('img/arrow-left.svg')}}" alt="frecciasx"></a>
+        <a href=""><img id="arrow-right" src="{{asset('img/arrow-right.svg')}}" alt="frecciadx"></a>
         <p>{!!$articolo['descrizione']!!}</p>
     </div>
 @endsection
