d64122d11ee720048297fc65133c2e27bf9abebf
Luca <lu.cucchi@gmail.com>
2020-05-07 19:56:37 +0200

    ok bonus

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -1,21 +1,32 @@
 @php
     $cards = config('products');
-    $articolo = $cards[$id];
+    $prodotto = $cards[$id];
 @endphp
 
 @extends('layouts.layout')
 
 @section('titolo')
-    {{$articolo['titolo']}}
+    {{$prodotto['titolo']}}
 @endsection
 
 @section('main')
     <div class="container2">
-        <h2>{{$articolo['titolo']}}</h2>
-        <img src="{{$articolo['src-h']}}" alt="{{$articolo['titolo']}}">
-        <img src="{{$articolo['src-p']}}" alt="{{$articolo['titolo']}}">
-        <a href=""><img id="arrow-left" src="{{asset('img/arrow-left.svg')}}" alt="frecciasx"></a>
-        <a href=""><img id="arrow-right" src="{{asset('img/arrow-right.svg')}}" alt="frecciadx"></a>
-        <p>{!!$articolo['descrizione']!!}</p>
+        <h2>{{$prodotto['titolo']}}</h2>
+        <img src="{{$prodotto['src-h']}}" alt="{{$prodotto['titolo']}}">
+        <img src="{{$prodotto['src-p']}}" alt="{{$prodotto['titolo']}}">
+        @if ($id >=1)
+            <a href="{{route('prodotto', $id - 1)}}"><img id="arrow-left" src="{{asset('img/arrow-left.svg')}}" alt="frecciasx"></a>
+        @else
+            <a href="{{route('prodotto', count($cards) - 1)}}"><img id="arrow-left" src="{{asset('img/arrow-left.svg')}}" alt="frecciasx"></a>
+        @endif
+
+        @if ($id == count($cards) -1)
+            <a href="{{route('prodotto', $id = 0)}}"><img id="arrow-right" src="{{asset('img/arrow-right.svg')}}" alt="frecciadx"></a>
+        @else
+            <a href="{{route('prodotto', $id + 1)}}"><img id="arrow-right" src="{{asset('img/arrow-right.svg')}}" alt="frecciadx"></a>
+        @endif
+
+
+        <p>{!!$prodotto['descrizione']!!}</p>
     </div>
 @endsection
