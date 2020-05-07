86e7fc2df4bc99e968df635582b47004b73dcc46
Luca <lu.cucchi@gmail.com>
2020-05-07 14:53:23 +0200

    added style

--- resources/views/prodotto.blade.php
+++ resources/views/prodotto.blade.php
@@ -10,7 +10,10 @@
 @endsection
 
 @section('main')
-    <img src="{{$articolo['src-h']}}" alt="">
-    <img src="{{$articolo['src-p']}}" alt="">
-    <p>{!!$articolo['descrizione']!!}</p>
+    <div class="container2">
+        <h2>{{$articolo['titolo']}}</h2>
+        <img src="{{$articolo['src-h']}}" alt="">
+        <img src="{{$articolo['src-p']}}" alt="">
+        <p>{!!$articolo['descrizione']!!}</p>
+    </div>
 @endsection
